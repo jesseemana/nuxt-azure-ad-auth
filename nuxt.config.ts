c0292f73fa6f122b51e0df66dd969bb63f7b973b
Jesse Emana <jesseemana@gmail.com>
2025-01-15 01:00:21 +0200

    FEAT: did stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+
+  devtools: { enabled: true },
+
+  modules: ['@sidebase/nuxt-auth'],
+
+  runtimeConfig: {
+    private: {
+      NUXT_SECRET: process.env.NUXT_SECRET,
+      AZURE_AD_CLIENT_ID: process.env.AZURE_AD_CLIENT_ID,
+      AZURE_AD_CLIENT_SECRET: process.env.AZURE_AD_CLIENT_SECRET,
+      AZURE_AD_TENANT_ID: process.env.AZURE_AD_TENANT_ID,
+      AZURE_AD_SCOPES: process.env.AZURE_AD_SCOPES
+    },
+    public: {}
+  },
+
+  auth: {
+    originEnvKey: process.env.ORIGIN,
+    provider: {
+      type: 'authjs',
+      defaultProvider: 'azure-ad',
+      addDefaultCallbackUrl: true
+    }
+  }
 })
